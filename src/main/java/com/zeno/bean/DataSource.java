56d9a1e2e18850e94250b5d00bc2837de89f1567
zeno <zhuyongit@163.com>
2018-01-05 12:15:26 +0800

    spring properties config

---
+++ src/main/java/com/zeno/bean/DataSource.java
@@ -0,0 +1,31 @@
+package com.zeno.bean;
+
+public class DataSource {
+
+    private String user;
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "DataSource{" +
+                "user='" + user + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/zeno/bean/PropertiesLoadConfigMain.java
@@ -0,0 +1,13 @@
+package com.zeno.bean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PropertiesLoadConfigMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("properties-context.xml");
+        DataSource source = (DataSource) context.getBean("dataSource");
+        System.out.println(source);
+    }
+}
