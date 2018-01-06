c0519834a01d5370cf27e7f98f1eaab77877b332
zeno <zhuyongit@163.com>
2018-01-06 08:46:15 +0800

    bean的生命周期指定与生命周期处理器

--- src/main/java/com/zeno/LifecycleBeanMain.java
+++ src/main/java/com/zeno/LifecycleBeanMain.java
@@ -5,6 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * bean 的生命周期与生命周期处理器
+ */
 public class LifecycleBeanMain {
 
     public static void main(String[] args) {
